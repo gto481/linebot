206a437f9b2637a784b3356ee6a2839080494a77
AlucardTulio <tul.kongprakaiwoot@ericsson.com>
2017-03-27 18:51:09 +0700

    sad

--- test.py
+++ test.py
@@ -2,30 +2,15 @@
 import json
 import sys
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
 
-
-bot = ChatBot("LineBot",
-    storage_adapter="chatterbot.storage.JsonFileStorageAdapter",
-    logic_adapters=[
-        "chatterbot.logic.MathematicalEvaluation",
-        "chatterbot.logic.TimeLogicAdapter",
-        "chatterbot.logic.BestMatch"
-    ],
-    input_adapter="chatterbot.input.TerminalAdapter",
-    output_adapter="chatterbot.output.TerminalAdapter",
-    database="../database.db"
+chatbot = ChatBot(
+    'Example Bot',
+    trainer='chatterbot.trainers.UbuntuCorpusTrainer'
 )
 
-print("Type something to begin...")
+# Start by training our bot with the Ubuntu corpus data
+chatbot.train()
 
-# The following loop will execute each time the user enters input
-while True:
-    try:
-        # We pass None to this method because the parameter
-        # is not used by the TerminalAdapter
-        bot_input = bot.get_response(None)
-
-    # Press ctrl-c or ctrl-d on the keyboard to exit
-    except (KeyboardInterrupt, EOFError, SystemExit):
-    	break
+# Now let's get a response to a greeting
+response = chatbot.get_response('How are you doing today?')
+print(response)
