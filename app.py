9d7a1c0daba954d1862f72b8c10a294fe7abc933
Tul Kongprakaiwoot <tul.kongprakaiwoot@ericsson.com>
2017-03-29 15:07:08 +0700

    fixed unicode

--- app.py
+++ app.py
@@ -95,8 +95,7 @@ def callback():
     for event in events:
         if event.type == 'message':
 
-            msg = event.message.text
-            msg = msg.decode('utf-8')
+            msg = event.message.text            
             #print msg
             response = bot.get_response(msg).text.encode('utf-8')
             #print response            
