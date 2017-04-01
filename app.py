cba2c74a1dc6b2bba13cc8cb9b4918e2922d6ada
tulio116 <lovetul@gmail.com>
2017-04-01 15:18:54 +0700

    fix search

--- app.py
+++ app.py
@@ -135,44 +135,44 @@ def googleSearch(text):
     g = google.search(text)
     columns = []
     i = 0
-    try:
-        for r in g:
-            i += 1
-            if ( i > 2):
-                break
-            if r is not None:
-	        print r.google_link
-	        url = r.google_link
-		title=r.name[:40]
-                text=r.description[:60]
-	        cc = CarouselColumn(text=text, title=title, actions=[URITemplateAction(label='Go to website', uri=url)])
-	        columns.append(cc)
-	        print columns
-        #    actions = [URITemplateAction(label='More Detail', uri=r.link)]
-        #     carousel_column = CarouselColumn(text=r.description.encode('utf-8'), title=r.name.encode('utf-8'), actions=actions)
-        #     columns.append(carousel_column)
+    #try:
+    for r in g:
+        i += 1
+        if ( i > 2):
+            break
+        if r is not None:
+            print r.google_link
+            url = r.google_link
+            title=r.name[:40]
+            text=r.description[:60]
+            cc = CarouselColumn(text=text, title=title, actions=[URITemplateAction(label='Go to website', uri=url)])
+            columns.append(cc)
+            print columns
+    #    actions = [URITemplateAction(label='More Detail', uri=r.link)]
+    #     carousel_column = CarouselColumn(text=r.description.encode('utf-8'), title=r.name.encode('utf-8'), actions=actions)
+    #     columns.append(carousel_column)
 
-        carousel_template = CarouselTemplate(columns=columns)
-        #carousel_template = CarouselTemplate(columns=columns)
-        #template_message = TemplateSendMessage(alt_text='Buttons alt text', template=carousel_template)
-        template_message = TemplateSendMessage(alt_text='Search result', template=carousel_template)
-        # carousel_template = CarouselTemplate(columns=[
-        #         CarouselColumn(text='hoge1', title='fuga1', actions=[
-        #             URITemplateAction(
-        #                 label='Go to line.me', uri='https://line.me'),
-        #             PostbackTemplateAction(label='ping', data='ping')
-        #         ]),
-        #         CarouselColumn(text='hoge2', title='fuga2', actions=[
-        #             PostbackTemplateAction(
-        #                 label='ping with text', data='ping',
-        #                 text='ping'),
-        #             MessageTemplateAction(label='Translate Rice', text='米')
-        #         ]),
-        #     ])
-        # template_message = TemplateSendMessage(alt_text='Buttons alt text', template=carousel_template)
-    except Exception:
-        response="ค้นหาไม่ได้วะ โทษที กูโง่"
-        template_message = TextSendMessage(text=response)
+    carousel_template = CarouselTemplate(columns=columns)
+    #carousel_template = CarouselTemplate(columns=columns)
+    #template_message = TemplateSendMessage(alt_text='Buttons alt text', template=carousel_template)
+    template_message = TemplateSendMessage(alt_text='Search result', template=carousel_template)
+    # carousel_template = CarouselTemplate(columns=[
+    #         CarouselColumn(text='hoge1', title='fuga1', actions=[
+    #             URITemplateAction(
+    #                 label='Go to line.me', uri='https://line.me'),
+    #             PostbackTemplateAction(label='ping', data='ping')
+    #         ]),
+    #         CarouselColumn(text='hoge2', title='fuga2', actions=[
+    #             PostbackTemplateAction(
+    #                 label='ping with text', data='ping',
+    #                 text='ping'),
+    #             MessageTemplateAction(label='Translate Rice', text='米')
+    #         ]),
+    #     ])
+    # template_message = TemplateSendMessage(alt_text='Buttons alt text', template=carousel_template)
+    #except Exception:
+    #    response="ค้นหาไม่ได้วะ โทษที กูโง่"
+    #    template_message = TextSendMessage(text=response)
     return template_message
 
 @app.route("/callback", methods=['POST'])
