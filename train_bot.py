1cb27562a8e1198e640628884151513cbe2cd4bf
tulio116 <lovetul@gmail.com>
2017-04-02 09:25:52 +0700

    change training

--- train_bot.py
+++ train_bot.py
@@ -26,7 +26,7 @@ bot = ChatBot('LineBot',
     database_uri='mongodb://bot:bot123@ds027425.mlab.com:27425/heroku_h80dpwn6'
 )
 
-def trainBot(text):
+def train(text):
      # Training bot with incoming message
     msglist = x.split(",")
     print msglist
