d51d84e9e1ff7dc00ce17df0a768fbb65ed7752f
gto481 <supicha_s@hotmail.com>
2017-04-04 22:37:11 +0700

    update ltf.py

--- ltf.py
+++ ltf.py
@@ -10,8 +10,11 @@ r = requests.get('http://www.thaifundstoday.com/en/funds?Fund+Type=ltf&tab=long&
 r.encoding = 'utf-8'
 
 final_col=[]
+new_cols=[]
+list_no=0
 
 soup = BeautifulSoup(r.content, 'lxml')
+#soup = BeautifulSoup(open("/tmp/out4.txt"), 'lxml')
 
 table = soup.find("table", id="fundsTable")
 
@@ -23,13 +26,16 @@ for row in rows:
 	cols=row.find_all('td')
 	num_field=1
 #	cols=[{(num_field):x.text.strip()} for x in cols]
-	new_cols=[]
 	for x in cols:
 		value="field"+str(num_field)
 		y=re.sub('[%]','',x.text)
-		new_cols.append({(value):y.strip()})
+		y1=y.strip()
+		#new_cols.append({(value):y.strip()})
+		if (num_field==1):
+			new_cols.append({(value):y1})
+		else: 
+			new_cols[(list_no)][(value)]=y1
 		num_field=num_field+1
+	list_no=list_no+1
 
-	final_col.append(new_cols)
-
-print json.dumps({'results': final_col})
+print json.dumps({'results': new_cols})
