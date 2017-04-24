668dbcb0398f1f7043e1584151486b455d1e4c1d
AlucardTulio <tul.kongprakaiwoot@ericsson.com>
2017-04-24 14:52:58 +0700

    add facebook support

--- app.py
+++ app.py
@@ -206,9 +206,58 @@ def parsingText(text):
     message = test_wit.parse(bot,text)
     return message
 
+
+def log(message):  # simple wrapper for logging to stdout on heroku
+    print str(message)
+    sys.stdout.flush()
+
+@app.route('/facebook', methods=['GET'])
+def verify():
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == "botme"
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Done", 200
+
+@app.route('/facebook', methods=['POST'])
+def webhook():
+
+    # endpoint for processing incoming messaging events
+
+    data = request.get_json()
+    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):  # someone sent us a message
+
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+
+                    log("SenderId: {}, RecipientId {}, text {}".format(sender_id,recipient_id,message_text))
+
+                if messaging_event.get("delivery"):  # delivery confirmation
+                    pass
+
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
+
+                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    pass
+
+    return "ok", 200
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers['X-Line-Signature']
+    X-Line-Signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
